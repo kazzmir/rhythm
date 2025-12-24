66a9204eaa475d31a22e891ac56955332d8c9882
Jon Rafkind <jon@rafkind.com>
2025-12-24 12:26:03 -0800

    load notes.mid

--- game/main/main.go
+++ game/main/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
-    _ "gitlab.com/gomidi/midi/v2/smf"
+    "log"
+
+    smflib "gitlab.com/gomidi/midi/v2/smf"
 )
 
 func main() {
+    smf, err := smflib.ReadFile("notes.mid")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Printf("Number of tracks: %d", len(smf.Tracks))
 }
